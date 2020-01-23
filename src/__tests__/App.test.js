91794da00c958f3705656edee229c26e84dd26ea
Mike Wong <wongmdesign@gmail.com>
2020-01-23 16:24:14 -0500

    first commit

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { shallow, mount } from "enzyme";
+
+import App from "../App";
+
+describe("App", () => {
+  it("renders", () => {
+    shallow(<App />);
+  });
+});
