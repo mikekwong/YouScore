85d3dadb2c47c471096d22cba7ccd752080a5781
Mike Wong <wongmdesign@gmail.com>
2020-01-25 15:03:36 -0500

    adjusting scores to show on click

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Player.css";
 
-const Player = ({ player, onPlayerSelect, isCorrect }) => {
+const Player = ({ player, onHandleClick, isCorrect, selectPlayer }) => {
   const {
     first_name,
     last_name,
@@ -13,14 +13,14 @@ const Player = ({ player, onPlayerSelect, isCorrect }) => {
   } = player;
 
   return (
-    <div onClick={e => onPlayerSelect(e)}>
-      <img src={url} alt="profile" />
-      <p>
+    <div>
+      <img src={url} alt="profile" onClick={e => onHandleClick(e)} />
+      <p onClick={e => onHandleClick(e)}>
         {first_name} {last_name}
       </p>
-      <p>{fppg.toFixed(1)}</p>
-      {/* {isCorrect && <p>{fppg.toFixed(1)}</p>} */}
-      {isCorrect && <p>Correct</p>}
+      {/* <p>{fppg.toFixed(1)}</p> */}
+      {/* {selectPlayer && <p>{fppg.toFixed(1)}</p>} */}
+      {/* {isCorrect ? <p>Correct</p> : <p>Wrong</p>} */}
     </div>
   );
 };
--- src/Players/Players.js
+++ src/Players/Players.js
@@ -7,60 +7,72 @@ import _ from "lodash";
 const Players = ({ fppgPlayers, setData }) => {
   const [isCorrect, setIsCorrect] = useState(false);
   const [correctGuesses, setCorrectGuesses] = useState(0);
+  const [selectPlayer, setSelectPlayer] = useState(false);
   // this hook to allow the component to re-render on call
   const [, setState] = useState();
 
   const playerGroup = () => {
     const randomPlayer = Math.floor(Math.random() * fppgPlayers.length - 6) + 0;
 
-    return fppgPlayers.splice(randomPlayer, 4);
+    return fppgPlayers.splice(randomPlayer, 2);
   };
 
   const players = playerGroup();
 
   console.log(fppgPlayers);
 
-  const onPlayerSelect = e => {
-    const highestScore = Math.max(...players.map(player => player.fppg));
+  const onHandleClick = e => {
     const { src, textContent } = e.target;
-    console.log(e.target.id);
+    const highestScore = Math.max(...players.map(player => player.fppg));
+    console.log("clicked");
+
     if (textContent && correctGuesses < 10) {
+      setSelectPlayer(true);
+
       if (
         _.filter(
           players,
           player => `${player.first_name} ${player.last_name}` === textContent
         )[0].fppg === highestScore
       ) {
-        setCorrectGuesses(correctGuesses + 1);
-        setIsCorrect(true);
+        // setCorrectGuesses(correctGuesses + 1);
+        // setIsCorrect(true);
       } else {
-        setIsCorrect(false);
+        // setIsCorrect(false);
       }
     }
     if (src && correctGuesses < 10) {
+      setSelectPlayer(true);
+
       if (
         _.filter(players, player => player.images.default.url === src)[0]
           .fppg === highestScore
       ) {
-        setCorrectGuesses(correctGuesses + 1);
-        setIsCorrect(true);
+        // setCorrectGuesses(correctGuesses + 1);
+        // setIsCorrect(true);
       } else {
-        setIsCorrect(false);
+        // setIsCorrect(false);
       }
     }
     // re-render component
-    setState({});
+    // setState({});
+    setSelectPlayer(false);
   };
 
   console.log(correctGuesses);
+  const playerNums = players.map(player => player.fppg);
+  console.log(playerNums);
 
+  console.log(selectPlayer);
   return (
     <div>
       {isCorrect ? <p>You win!</p> : <p>You lose!</p>}
+      {selectPlayer && <p>{playerNums[0].toFixed(1)}</p>}
       {_.map(players, player => {
         return (
           <Player
-            onPlayerSelect={onPlayerSelect}
+            onHandleClick={onHandleClick}
+            selectPlayer={selectPlayer}
             key={player.id}
             player={player}
             isCorrect={isCorrect}
