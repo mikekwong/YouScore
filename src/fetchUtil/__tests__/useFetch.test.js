d3d8a3ff707707442ca03e0a18d371878d0f7d96
Mike Wong <wongmdesign@gmail.com>
2020-01-30 15:59:53 -0500

    m updated css

--- src/fetchUtil/__tests__/useFetch.test.js
+++ src/fetchUtil/__tests__/useFetch.test.js
@@ -1,6 +1,6 @@
 import { renderHook } from "@testing-library/react-hooks";
-import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
 
 import useFetch from "../useFetch";
 
